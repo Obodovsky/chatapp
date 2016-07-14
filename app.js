2086b1dca6dd0051e77480025af98af79f0d938b
Супер-васька <obod@mail.ru>
2016-07-14 16:38:09 +0300

    Установил express -debug и morgan для отладки

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ var app =express();
 
 var BodyParser = require("body-parser");
 
+var fs= require("fs");
+
+//Подключаем morgan и выводим пути в access.log
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+app.use(require("morgan")("combined", {stream: accessLogStream}));
+
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("node_modules/jquery/dist"));
+
+// Дебаггер
+require("express-debug")(app,{});
+
+
 app.use(BodyParser.urlencoded({extended: true}));
 app.use(BodyParser.json());
 
 app.set('view engine', 'jade');
 app.set('views', './views');
+
+
 
 app.get('/', function (req, res) {
     res.render('home', {
